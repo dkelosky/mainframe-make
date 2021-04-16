942fc95acab59cc721ad96bbbdfd251343ae2a80
Dan Kelosky <dkelosky@gmail.com>
2021-04-16 10:47:55 -0400

    dynalloc
    Signed-off-by: Dan Kelosky <dkelosky@gmail.com>

--- zossrc/lib/run.cpp
+++ zossrc/lib/run.cpp
@@ -1,12 +1,31 @@
 #include <iostream>
+#include <stdlib.h>
 #include "runexec.h"
 
 using namespace std;
 
-int main() {
+#pragma runopts(trap(on, nospie))
+
+// typedef int EXTF(char *);
+extern "OS_UPSTACK" typedef int EXTF(char *);
+// #pragma linkage(EXTF, OS_UPSTACK)
+// #pragma linkage(EXTF, OS)
+
+int call_alloc();
+
+
+int main()
+{
     cout << "hello" << endl;
 
     int i = RUNEXE();
+    call_alloc();
 
     cout << "hello " << i << endl;
+}
+
+int call_alloc()
+{
+    EXTF *bpxwdyn = (EXTF *)fetch("BPXWDYN");
+    return bpxwdyn("alloc fi(syslib) da(sys1.maclib) shr msg(2)");
 }
--- zossrc/lib/runexec.c
+++ zossrc/lib/runexec.c
@@ -1,8 +1,19 @@
 #include "runexec.h"
+#include <stdlib.h>
 
 #pragma prolog(RUNEXE, "&CCN_MAIN SETB 1 \n MYPROLOG")
 
 int RUNEXE()
 {
+    // call_alloc();
     return 3;
 }
+
+// typedef int EXTF();
+// #pragma linkage(EXTF, OS_UPSTACK)
+
+// int call_alloc()
+// {
+//     EXTF *bpxwdyn = (EXTF *)fetch("BPXWDYN");
+//     return bpxwdyn("alloc fi(syslib) da(sys1.maclib) shr msg(2)");
+// }
