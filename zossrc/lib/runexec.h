d99a79c856a05251304398e305b40cf6c247446e
Dan Kelosky <dkelosky@gmail.com>
2021-04-16 09:19:58 -0400

    clean header
    Signed-off-by: Dan Kelosky <dkelosky@gmail.com>

--- zossrc/lib/runexec.h
+++ zossrc/lib/runexec.h
@@ -1,14 +1,8 @@
 #ifndef RUNEXEC_H
 #define RUNEXEC_H
 
-/**
- * C/C++ interlanguage - "OS" meaning to call non-Language Environment conforming assembler.
- */
-#if defined(__cplusplus) && (defined(__IBMCPP__) || defined(__IBMC__))
+#if defined(__cplusplus)
 extern "OS"
-{
-#elif defined(__cplusplus)
-extern "C"
 {
 #endif
 
